adaac439f60ae2429c64456f3aa21b71ec1376c8
JR Bussard <scottbussardjr@gmail.com>
2025-01-03 05:39:19 -0500

    Added the contact page and added the styling for it

--- src/contact.js
+++ src/contact.js
@@ -2,3 +2,18 @@
 // - person card x 3
 
 import { PageHeading, PersonCard } from "./components";
+
+export class ContactPage {
+    constructor() {
+        this.heading = new PageHeading("Contact Us");
+        this.billybob = new PersonCard("Billy Bob", "CEO", "(123) 456-7890", "billybob@email.com");
+        this.bobbilly = new PersonCard("Bob Billy", "CTO", "(098 765-4321)", "bobbilly@email.com");
+
+        this.container = document.createElement("div");
+        this.container.className = "contact";
+
+        this.container.appendChild(this.heading.modal);
+        this.container.appendChild(this.billybob.modal);
+        this.container.appendChild(this.bobbilly.modal);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
 import { HomePage } from "./home";
 import { MenuPage } from "./menu";
+import { ContactPage } from "./contact";
 
 const home = new HomePage();
 const menu = new MenuPage();
+const contact = new ContactPage();
 const container = document.querySelector("#content");
 
 const buttons = document.querySelectorAll("nav");
 
-container.appendChild(menu.container);
+container.appendChild(contact.container);
